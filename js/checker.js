2cd9b36d20e0143df0b7caf363434d7e4c23fbef
yamareping <yamareping@gmail.com>
2014-05-18 21:30:27 +0800

    commit3

--- js/checker.js
+++ js/checker.js
@@ -19,7 +19,7 @@ $.getJSON('http://jsbin.com/jaziroja/1', {}, function(data){
 // 設定 Facebook AppID
 window.fbAsyncInit = function(){
   FB.init({
-    appId: '708865552487902', // 若可以，請換成自己的 App ID !
+    appId: '313146792174304', // 若可以，請換成自己的 App ID !
     status: true
   });
 
